2b69e06435eb8aa32fb9480c0391d25938f4d1da
Raissa Ferreira <rai200890@gmail.com>
2016-10-13 01:17:02 -0300

    Add create and delete bookmark functionalities

--- app/controllers/bookmark.list.controller.js
+++ app/controllers/bookmark.list.controller.js
@@ -1,11 +1,63 @@
 function BookmarkListController(Bookmark) {
-    var ctrl = this;
-    ctrl.bookmarks = [];
+  var ctrl = this;
+  ctrl.bookmarks = [];
+  ctrl.pagination = {
+    per_page: 15,
+    page: 1
+  };
 
-    //init
-    Bookmark.index(1, 15).success(function(response){
-      ctrl.bookmarks = response;
+  ctrl.newBookmark = {};
+  ctrl.alerts = [];
+
+  ctrl.closeAlert = function(index) {
+    ctrl.alerts.splice(index, 1);
+  };
+
+  ctrl.create = function() {
+    Bookmark.create(ctrl.newBookmark)
+      .success(function(response) {
+        ctrl.alerts.push({
+          "type": "success",
+          "messages": ["Bookmark successfully created!"]
+        });
+        loadBookmarks();
+      })
+      .error(function(response) {
+        ctrl.alerts.push({
+          "type": "danger",
+          "messages": response.errors
+        });
+      });
+  };
+
+  ctrl.delete = function(bookmark) {
+    Bookmark.delete(bookmark.id)
+    .success(function(){
+      ctrl.alerts.push({
+        "type": "success",
+        "messages": ["Bookmark " + bookmark.id + " successfully deleted!"]
+      });
+      loadBookmarks();
+    })
+    .error(function(response){
+      ctrl.alerts.push({
+        "type": "danger",
+        "messages": response.errors
+      });
     });
+
+  };
+
+  loadBookmarks = function() {
+    Bookmark.index(ctrl.pagination.page, ctrl.pagination.per_page)
+      .success(function(response) {
+        ctrl.bookmarks = response.bookmarks;
+        ctrl.pagination = response.pagination;
+      });
+  };
+
+  loadBookmarks();
+
 };
 BookmarkListController.$inject = ['Bookmark'];
 
