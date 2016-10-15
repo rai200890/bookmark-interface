d65bfca9c57ad280900547394e731ef7c47231d3
Raissa Ferreira <rai200890@gmail.com>
2016-10-15 00:49:15 -0300

    Refactor login redirection when token invalid

--- app/app.run.js
+++ app/app.run.js
@@ -1,12 +1,9 @@
-function run(Auth, $rootScope, $state, authManager) {
-  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-    if (!Auth.isTokenValid() && $state.current.name !== 'signup') {
-      $state.go('login');
-    };
-
-    authManager.checkAuthOnRefresh();
-    authManager.redirectWhenUnauthenticated();
-  });
-};
-run.$inject = ['Auth', '$rootScope', '$state', 'authManager', 'jwtHelper'];
+function run(Auth, $state, authManager) {
+  if (!Auth.isTokenValid() && $state.current.name !== 'signup') {
+    $state.go('login');
+  };
+  authManager.checkAuthOnRefresh();
+  authManager.redirectWhenUnauthenticated();
+}
+run.$inject = ['Auth', '$state', 'authManager'];
 module.exports = run;
