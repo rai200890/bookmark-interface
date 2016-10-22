e0f272c00f569d6e36c771f12120471eda9d24c7
Raissa Ferreira <rai200890@gmail.com>
2016-10-22 12:54:48 -0200

    Hide password field on edit user view
    - Add a change password button to display it

--- app/controllers/user.edit.controller.js
+++ app/controllers/user.edit.controller.js
@@ -1,21 +1,27 @@
 function UserEditController(User, $stateParams) {
   var ctrl = this;
   ctrl.user = null;
+  ctrl.passwordToBeChanged = false;
   ctrl.alerts = [];
 
-  ctrl.edit = function(user) {
-    User.update(user.id, user)
+  ctrl.save = function() {
+    User.update(ctrl.user.id, ctrl.user)
       .success(function() {
         ctrl.alerts.push({
           "type": "success",
-          "messages": ["User " + user.id + " successfully updated!"]
+          "messages": ["User " + ctrl.user.id + " successfully updated!"]
         });
+        ctrl.passwordToBeChanged = false;
       }).error(function(response) {
         ctrl.alerts.push({
           "type": "danger",
           "messages": response.errors
         });
       });
+  };
+
+  ctrl.changePassword = function(){
+    ctrl.passwordToBeChanged = true;
   };
 
   User.show($stateParams.id).success(function(response) {
